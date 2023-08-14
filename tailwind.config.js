496e2bd774b9c8fbe417bbfecf922ff599c0f1dd
Ash-KODES <choudharykumarashutosh1209@gmail.com>
2023-08-14 11:12:47 +0530

    final draft

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./node_modules/flowbite-react/**/*.js",
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      gridTemplateColumns: {
+        26: "repeat(26, minmax(0,1fr))",
+      },
+      gridTemplateRows: {
+        10: "repeat(10, minmax(0,1fr))",
+      }
+    },
+    fontFamily: {
+      'open-sans': ['Open Sans', 'sans-serif']
+    }
+  },
+  plugins: [require("daisyui"),require("flowbite"),require("tailwind-scrollbar-hide")],
+
+}
